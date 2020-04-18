75e7f3655a981e46f736e49df299a4d1049d795d
Andrew Khan <iquach@icloud.com>
2020-04-18 21:22:29 +0300

    done homework register forms

--- register.php
+++ register.php
@@ -1,7 +1,10 @@
 <?php
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
+    $lang = [
+        'ru' => 'Русский',
+        'ua' => 'Український',
+        'eng' => 'English',
+        'fr' => 'French',
+    ];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,21 +15,35 @@
 </head>
 <body>
 <br />
-<?php if ($_POST) : ?>
+<?php if (!empty($_POST)) : ?>
    <div class="container">
+       <h1>Данные о регистрации</h1>
        <ul class="list-group mb-3">
            <li class="list-group-item">Имя: <?php echo $_POST['name'] ?></li>
            <li class="list-group-item">Фамилия: <?php echo $_POST['surname'] ?></li>
            <li class="list-group-item">Логин: <?php echo $_POST['login'] ?></li>
            <li class="list-group-item">Пароль: <?php echo $_POST['password'] ?></li>
-           <li class="list-group-item">Пол: <?php echo $_POST['gender'] ?></li>
-           <li class="list-group-item">Город: <?php echo $_POST['city'] ?></li>
+           <li class="list-group-item">Почта: <?php echo $_POST['email'] ?></li>
+           <?php if ($_POST['gender']) : ?>
+               <li class="list-group-item">Пол: <?php echo $_POST['gender'] ?></li>
+           <?php endif; ?>
+           <?php if ($_POST['city']) : ?>
+               <li class="list-group-item">Город: <?php echo $_POST['city'] ?></li>
+           <?php endif; ?>
+           <?php if ($_POST['lang']) : ?>
+               <li class="list-group-item">Язык:
+                   <?php foreach ($_POST['lang'] as $item) : ?>
+                        <?php echo $item ?>
+                   <?php endforeach; ?>
+               </li>
+           <?php endif; ?>
        </ul>
    </div>
 <?php endif; ?>
 
 <div class="container">
     <form method="post" action="register.php">
+        <h1>Форма Регистрации</h1>
         <div class="form-group">
             <div class="row">
                 <div class="col">
@@ -39,11 +56,11 @@
                 </div>
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group is-valid">
             <label for="login">Логин</label>
             <input name="login" type="text" class="form-control" id="login" placeholder="Your Login" value="<?php echo $_POST['login']; ?>" required>
         </div>
-        <div class="form-group">
+        <div class="form-group is-valid">
             <label for="password">Пароль</label>
             <input name="password" type="password" class="form-control" id="password" placeholder="Your Password" value="<?php echo $_POST['password']; ?>" required>
         </div>
@@ -51,23 +68,41 @@
             <label for="email">Почта</label>
             <input name="email" type="email" class="form-control" id="email" placeholder="Your Email" value="<?php echo $_POST['email']; ?>" required>
         </div>
-        <div class="form-group">
-            <label for="gender">Выбирите пол:</label>
-            <select class="form-control" id="gender" name="gender">
-                <option>Мужчина</option>
-                <option>Женшина</option>
-            </select>
+        <div class="row">
+            <div class="col">
+                <div class="form-group">
+                    <label for="gender">Выбирите пол:</label>
+                    <select class="form-control" id="gender" name="gender">
+                        <option selected disabled>Выбирите пол</option>
+                        <option>Мужчина</option>
+                        <option>Женшина</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label for="city">Город</label>
+                    <select class="form-control" id="city" name="city">
+                        <option selected disabled>Выбирите город проживания</option>
+                        <option>Полтава</option>
+                        <option>Киев</option>
+                        <option>Одесса</option>
+                        <option>Львов</option>
+                        <option>Чернигов</option>
+                    </select>
+                </div>
+            </div>
         </div>
         <div class="form-group">
-            <label for="city">Город</label>
-            <select class="form-control" id="city" name="city">
-                <option>Полтава</option>
-                <option>Киев</option>
-                <option>Одесса</option>
-                <option>Львов</option>
-                <option>Чернигов</option>
+            <label for="lang">Язык:</label>
+            <select class="form-control" id="lang" name="lang[]" size="5" multiple>
+                <option selected disabled>Выбирите язык</option>
+                <?php foreach ($lang as $item) : ?>
+                    <option><?php echo $item; ?></option>
+                <?php endforeach; ?>
             </select>
         </div>
+
         <button type="submit" class="btn btn-primary">Отправить</button>
     </form>
 </div>
