301c614a1737a50efde56dc4197eb31b9f0538f3
Andrew Khan <iquach@icloud.com>
2020-04-22 19:13:28 +0300

    done

--- user.php
+++ user.php
@@ -1,63 +1,109 @@
 <?php
-    $value = 'Hello form PHP 7.2';
-    $name = 'Andrii';
-    $surname = 'Khan';
-    $age = 28;
-    $email = 'iquach@icloud.com';
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
+$error = [];
+if (!empty($_POST)) {
+    if (empty($_POST['name'])) {
+        $error['name'] = 'Имя не может пустым';
+    }
+    if (empty($_POST['surname'])) {
+        $error['surname'] = 'Фамилия не может пустой';
+    }
+    if (empty($_POST['age']) || $_POST['age'] < 1) {
+        $error['age'] = 'Возраст задан некорректно';
+    }
+    if (empty($_POST['email'])) {
+        $error['email'] = 'Почта задана некорректно';
+    }
+}
+$lang = (!empty($_GET['lang'])) ? $_GET['lang'] : 'ru';
+$labels = [
+        'ru' => ['name' => 'Имя:', 'surname' => 'Фамилия:', 'age' => 'Возраст:', 'email' => 'Почта:', 'gender' => 'Выберите Пол:', 'send' => 'Отправить', 'title' => 'Форма Регистрации'],
+        'ua' => ['name' => "Ім'я", 'surname' => 'Призвіще:', 'age' => 'Вік:', 'email' => 'Пошта:', 'gender' => 'Оберіть Стать:', 'send' => 'Відправити','title' => 'Форма Реєстрації'],
+        'en' => ['name' => 'Name:', 'surname' => 'Surname:', 'age' => 'Age:', 'email' => 'Email:', 'gender' => 'Select Gender:', 'send' => 'Send','title' => 'Registration Form'],
+];
+
+switch ($lang) {
+    case 'ru';
+        $translation = $labels['ru'];
+        break;
+    case 'ua';
+        $translation = $labels['ua'];
+        break;
+    case 'en';
+        $translation = $labels['en'];
+        break;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Форма</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title><?php echo $translation['title']; ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
-<br />
-<?php if ($_POST['name'] && $_POST['city']) : ?>
-    <h1>Ваше Имя: <?php echo $_POST['name'] ?>. Ваш город: <?php echo $_POST['city'][0]; ?></h1>
-<?php endif; ?>
-
+<br/>
 <div class="container">
+    <h1><?php echo $translation['title']; ?></h1>
+</div>
+<div class="container">
+    <div class="float-right">
+        <a href="?lang=ru" class="badge badge-primary">Русский</a>
+        <a href="?lang=ua" class="badge badge-secondary">Украинский</a>
+        <a href="?lang=en" class="badge badge-success">Английский</a>
+    </div>
     <form method="post" action="user.php">
         <div class="form-group">
-            <label for="name">Имя и Фамилия</label>
-            <input name="name" type="text" class="form-control" id="name" placeholder="Your Name" value="<?php echo $_POST['name'] ?? 'Андрей'; ?>">
+            <label for="name"><?php echo $translation['name']; ?></label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Введите Имя"
+                   <?php if ($_POST['name']): ?>value="<?php echo $_POST['name'] ?>"<?php endif; ?>>
+            <?php if (!empty($error['name'])) : ?>
+                <small class="text-danger">
+                    <?php echo $error['name'] ?>
+                </small>
+            <?php endif; ?>
         </div>
         <div class="form-group">
-            <label for="surname">Фамилия</label>
-            <input name="surname" type="text" class="form-control" id="surname" placeholder="Your Surname" value="<?php echo $_POST['surname'] ?? 'Хан'; ?>">
+            <label for="surnme"><?php echo $translation['surname']; ?></label>
+            <input type="text" class="form-control" id="surnme" name="surname"
+                   placeholder="Введите Фамилию"
+                   <?php if ($_POST['surname']): ?>value="<?php echo $_POST['surname'] ?>"<?php endif; ?>">
+            <?php if (!empty($error['surname'])) : ?>
+                <small class="text-danger">
+                    <?php echo $error['surname'] ?>
+                </small>
+            <?php endif; ?>
         </div>
         <div class="form-group">
-            <label for="age">Возраст</label>
-            <input name="age" type="number" class="form-control" id="age" placeholder="Your Age" value="<?php echo $_POST['age']; ?>">
+            <label for="age"><?php echo $translation['age']; ?></label>
+            <input type="number" class="form-control" id="age" name="age" placeholder="Введите Возраст"
+                   <?php if ($_POST['age']): ?>value="<?php echo $_POST['age'] ?>"<?php endif; ?>>
+            <?php if (!empty($error['age'])) : ?>
+                <small class="text-danger">
+                    <?php echo $error['age'] ?>
+                </small>
+            <?php endif; ?>
         </div>
         <div class="form-group">
-            <label for="email">Почта</label>
-            <input name="email" type="email" class="form-control" id="email" placeholder="Your Email" value="<?php echo $_POST['email']; ?>">
+            <label for="email"><?php echo $translation['email']; ?></label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Введите Email"
+                   <?php if ($_POST['email']): ?>value="<?php echo $_POST['email'] ?>" <?php endif; ?>>
+            <?php if (!empty($error['email'])) : ?>
+                <small class="text-danger">
+                    <?php echo $error['email'] ?>
+                </small>
+            <?php endif; ?>
         </div>
         <div class="form-group">
-            <label for="gender">Выбирите пол:</label>
+            <label for="gender"><?php echo $translation['gender']; ?></label>
             <select class="form-control" id="gender" name="gender">
-                <option>Мужчина</option>
-                <option>Женшина</option>
-                <option>Другое</option>
+                <option disabled selected>Выберите пол</option>
+                <option <?php echo $_POST['gender'] == 'Man' ? 'selected' : '' ?>>Man</option>
+                <option <?php echo $_POST['gender'] == 'Woman' ? 'selected' : '' ?>>Woman</option>
+                <option <?php echo $_POST['gender'] == 'Other' ? 'selected' : '' ?>>Others</option>
             </select>
         </div>
-        <div class="form-group">
-            <label for="city">Город</label>
-            <select class="form-control" id="city" name="city[]" multiple>
-                <option>Полтава</option>
-                <option>Киев</option>
-                <option>Одесса</option>
-                <option>Львов</option>
-                <option>Чернигов</option>
-            </select>
-        </div>
-        <button type="submit" class="btn btn-primary">Отправить</button>
+        <button type="submit" class="btn btn-primary"><?php echo $translation['send']; ?></button>
     </form>
 </div>
 </body>
