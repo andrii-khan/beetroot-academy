b16a46b05f639b4f67fa2ad8e7158926d112c3ac
Andrew Khan <iquach@icloud.com>
2020-05-16 21:24:31 +0300

    config mysql

--- db/environment/bootstrap.sh
+++ db/environment/bootstrap.sh
@@ -6,4 +6,6 @@ apt install -y php
 apt install -y mysql-server
 apt install php-xdebug
 cp /var/www/html/environment/xdebug.ini /etc/php/7.2/cli/conf.d/20-xdebug.ini
+cp /var/www/html/environment/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 systemctl reload apache2
+systemctl restart mysql
